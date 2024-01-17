d8986057269702a3a6bc5a0c7809dffa810b2466
unknown <amienulrana@gmail.com>
2024-01-18 00:08:31 +0700

    create middleware for protecting route

---
+++ src/middleware.js
@@ -0,0 +1,21 @@
+// middleware.js
+
+import { NextResponse } from 'next/server';
+
+export default function middleware(req) {
+  let loggedin = req.cookies.get('userSession');
+  console.log(loggedin);
+  const { pathname } = req.nextUrl;
+
+  if (loggedin && pathname === '/') {
+    return NextResponse.redirect(new URL('/profile', req.url));
+  }
+  if (!loggedin && pathname === '/profile') {
+    console.log('belum login');
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+}
+
+export const config = {
+  matcher: '/((?!api|static|.*\\..*|_next).*)',
+};
