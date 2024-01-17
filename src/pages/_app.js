7ddf69a75636d5926c3503264d5194c18cb61b14
unknown <amienulrana@gmail.com>
2024-01-18 00:12:34 +0700

    render Toaster from react-hot-toastify

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,6 +2,15 @@ import '@/styles/globals.css'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
+import { Provider } from 'react-redux';
+import store from '@/lib/store';
+import { Toaster } from 'react-hot-toast';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+      <Toaster />
+    </Provider>
+  )
 }
