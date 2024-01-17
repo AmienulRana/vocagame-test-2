6cd7e5e26ec15b6c5f11b2bb25b0aa62d47b5099
unknown <amienulrana@gmail.com>
2024-01-18 03:09:45 +0700

    create responsive in image footer

--- src/components/commons/Footer.js
+++ src/components/commons/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <div className="w-full mt-14 px-24 h-[338px] bg-lightgreen dark:bg-darkblue2 pt-5">
         <div className="mx-auto max-w-[1400px]">
-          <Image src="/svg/logo-white.svg" width={150} height={150} alt="logo" className="mt-5 block" />
+          <Image src="/svg/logo-white.svg" width={150} height={150} alt="logo" className="mt-5 block w-24 md:w-auto" />
         </div>
     </div>
   );
