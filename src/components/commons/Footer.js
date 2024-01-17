d9e78962d2688cf70964c9c7e4aa2e4730903886
unknown <amienulrana@gmail.com>
2024-01-18 00:10:47 +0700

    create max width in footer

--- src/components/commons/Footer.js
+++ src/components/commons/Footer.js
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <div className="w-full mt-14 px-24 h-[338px] bg-lightgreen pt-5">
-      <Image src="/svg/logo-white.svg" width={150} height={150} alt="logo" className="mt-5 block" />
+        <div className="mx-auto max-w-[1400px]">
+          <Image src="/svg/logo-white.svg" width={150} height={150} alt="logo" className="mt-5 block" />
+        </div>
     </div>
   );
 }
