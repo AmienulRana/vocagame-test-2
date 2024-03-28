7acb93add703ea870afb57c5eabad389f9ec4c30
unknown <amienulrana@gmail.com>
2024-03-28 11:51:26 +0700

    remove unused code in middleware

--- src/middleware.js
+++ src/middleware.js
@@ -4,14 +4,12 @@ import { NextResponse } from 'next/server';
 
 export default function middleware(req) {
   let loggedin = req.cookies.get('userSession');
-  console.log(loggedin);
   const { pathname } = req.nextUrl;
 
   if (loggedin && pathname === '/') {
     return NextResponse.redirect(new URL('/profile', req.url));
   }
   if (!loggedin && pathname === '/profile') {
-    console.log('belum login');
     return NextResponse.redirect(new URL('/login', req.url));
   }
 }
