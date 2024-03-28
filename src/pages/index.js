25f0ce62721a106c6680b3e3123271a2087d06c1
unknown <amienulrana@gmail.com>
2024-03-28 11:51:47 +0700

    use typhography for the title

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,7 @@ import Link from "next/link";
 import Typhography from "@/components/commons/Typhography";
 import FormRegister from "@/components/FormRegister";
 
-
 export default function Register() {
-  
   return (
     <div className="flex-1 min-h-screen flex justify-center items-center overflow-hidden relative dark:bg-darkblue">
       <OvalVector />
@@ -19,16 +17,14 @@ export default function Register() {
         className="absolute md:top-10 md:left-8 top-5 left-6 md:w-24 w-16"
       />
       <div className="px-8 max-w-[900px] lg:w-1/2 md:w-[70%] w-full md:mx-auto mt-10  flex flex-col items-center justify-center h-full">
-        <h1 className="text-lightgreen xl:text-[64px] lg:text-[32px] text-[28px] font-bold">
-          Daftarkan Akun
-        </h1>
-        <Typhography variant={'p'} className="mt-5 text-gray-400 mb-10">
+        <Typhography variant={"h1"}>Daftarkan Akun</Typhography>
+        <Typhography variant={"p"} className="mt-5 text-gray-400 mb-10">
           Daftar akun anda dengan mengisi form dibawah
         </Typhography>
 
         <FormRegister />
 
-        <Typhography variant={'p'} className="mb-10 z-50">
+        <Typhography variant={"p"} className="mb-10 z-50">
           Sudah punya akun?{" "}
           <Link href="/login" className="text-lightgreen font-bold">
             Login Sekarang
