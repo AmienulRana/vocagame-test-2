fce4ebc06db6ce3c7a527f45a3a87ac65c2c0df2
unknown <amienulrana@gmail.com>
2024-03-28 11:50:59 +0700

    add new variant of h1

--- src/components/commons/Typhography.js
+++ src/components/commons/Typhography.js
@@ -1,5 +1,6 @@
 const style = {
     p: 'text-gray-400 dark:text-white 2xl:text-xl',
+    h1: 'text-lightgreen xl:text-[64px] md:text-[32px] text-[28px] font-bold'
 }
 
 export default function Typhography({variant, className, children}){
